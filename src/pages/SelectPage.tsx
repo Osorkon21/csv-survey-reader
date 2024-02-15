3b053b81928fceb1055de94f35d2604141b3c126
Osorkon21 <Osorkon21@users.noreply.github.com>
2024-02-15 01:55:26 -0600

    Add more formatting logic

--- src/pages/SelectPage.tsx
+++ src/pages/SelectPage.tsx
@@ -6,14 +6,19 @@ interface SelectProps {
 
 export default function SelectPage({ content }: SelectProps) {
 
-  /** count, line where value present */
-  type CountAndLine = {
+  /** question index, line where value present */
+  interface QuestionAndLine {
+    questionIndex: number,
+    line: number
+  }
+
+  /** count, question index, line where value present */
+  interface CountQuestionLine extends QuestionAndLine {
     count: number,
-    lineLoc: number
   };
 
   /** count, lines where value present */
-  type CountAndLines = {
+  interface CountQuestionLines {
     count: number,
     lines: number[]
   };
@@ -40,53 +45,98 @@ export default function SelectPage({ content }: SelectProps) {
   const [phrases, setPhrases] = useState([""])
 
   /** phrase, line number */
-  const phraseMap = new Map<string, number>();
+  const phraseMap = new Map<string, QuestionAndLine>();
 
   /** word, [count, line locations] */
-  const wordMap = new Map<string, CountAndLines>();
+  const wordMap = new Map<string, CountQuestionLines>();
 
   /**
-   * Filters empty strings and phrases that are mostly numbers
-   * @param arr string array to filter
-   * @returns filtered string array
+   * Determines whether the string should be filtered out
+   * @param arr string to check
+   * @returns whether string should be kept
    */
-  function firstFilter(arr: string[]): string[] {
-    arr = arr.filter((item) => item !== "")
+  function shouldAdd(str: string): boolean {
+    if (str === "")
+      return false;
 
-    return arr.filter((item) => {
-      const letterCount = item.replace(/[^a-zA-Z]/g, '').length;
-      const digitCount = item.replace(/[a-zA-Z]/g, '').length;
-      return letterCount >= digitCount;
-    });
+    const letterCount = str.replace(/[^a-zA-Z]/g, '').length;
+    const digitCount = str.replace(/[a-zA-Z]/g, '').length;
+    return letterCount >= digitCount;
+  }
+
+  function formatSplitLine(arr: string[], line: number): Map<string, QuestionAndLine> {
+    let map = new Map<string, QuestionAndLine>();
+
+    let questionIndex = 0;
+    let quoteDetected = false;
+    let tempString = "";
+
+    for (let str of arr) {
+
+      if (quoteDetected) {
+        if (str[str.length - 1] === "\"" && str[str.length - 2] !== "\"") {
+          tempString += str.slice(0, -1);
+          quoteDetected = false;
+        }
+        else
+          tempString += str;
+      }
+      else if (str[0] === "\"" && str[1] !== "\"") {
+        quoteDetected = true;
+        tempString = str.slice(1);
+      }
+
+      if (!quoteDetected) {
+        if (tempString !== "") {
+          if (shouldAdd(tempString)) {
+            map.set(tempString, { questionIndex: questionIndex, line: line })
+          }
+          tempString = "";
+        }
+        else if (shouldAdd(str)) {
+          map.set(str, { questionIndex: questionIndex, line: line })
+        }
+
+        questionIndex++;
+      }
+    }
+
+    return map;
   }
 
   function processContent(content: string) {
 
-    /** phrase, [count, line number] */
-    let phraseCounter = new Map<string, CountAndLine>();
+    /** phrase, {count, question index, line number} */
+    let phraseCounter = new Map<string, CountQuestionLine>();
 
     let lines = content.replace(/\r\n/g, "\n").split("\n");
 
+    // add questions from first line of file to a new QuestionMap
+
     // ignore survey questions on first line
     for (let i = 1; i < lines.length; i++) {
-      let filteredPhrases = firstFilter(lines[i].split(","));
+      let splitLine = lines[i].split(",");
 
-      for (let phrase of filteredPhrases) {
-        let phraseCount = phraseCounter.get(phrase);
+      let formattedLines = formatSplitLine(splitLine, i + 1);
 
-        if (phraseCount !== undefined) {
-          phraseCounter.set(phrase, { ...phraseCount, count: phraseCount.count + 1 });
-        }
-        else
-          phraseCounter.set(phrase, { count: 1, lineLoc: i + 1 });
-      }
+      console.log(formattedLines)
+
+      // for (let [phrase, qal] of formattedPhrases) {
+      //   let phraseCount = phraseCounter.get(phrase);
+
+      //   if (phraseCount !== undefined) {
+      //     phraseCounter.set(phrase, { ...phraseCount, count: phraseCount.count + 1 });
+      //   }
+      //   else
+      //     phraseCounter.set(phrase, { questionIndex: qal.questionIndex, count: 1, line: i + 1 });
+      // }
     }
 
-    for (let [phrase, cal] of phraseCounter) {
-      if (cal.count === 1) {
-        phraseMap.set(phrase, cal.lineLoc)
-      }
-    }
+    // for (let [phrase, cql] of phraseCounter) {
+    //   if (cql.count === 1) {
+    //     phraseMap.set(phrase, { questionIndex: cql.questionIndex, line: cql.line })
+    //   }
+    // }
 
     // console.log(phraseMap);
 
